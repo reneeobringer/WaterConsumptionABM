b370c76208a070c3cac893b3449da668875e50d9
Renee Obringer <102612063+reneeobringer@users.noreply.github.com>
2022-05-26 14:20:38 -0400

    Add files via upload

--- ABM_supplementalcalc.R
+++ ABM_supplementalcalc.R
@@ -11,7 +11,7 @@
 # DATA PRE-PROCESSING: spatiotemporal aggregation, unit conversion, etc.
 # WATER CONSUMPTION CALCULATION: using Wang et al. 2021, calculate water consumption for all survey respondents
 # FIT DISTRIBUTIONS: fit distirbutions for all hydrocliamtic variables and water consumption
-# FIGURES: code for plotting figures and creating tables included in manuscript 
+# FIGURES AND TABLES: code for plotting figures and creating tables included in manuscript 
 
 rm(list=ls())
 options(scipen = 999)
@@ -23,6 +23,7 @@ library(reshape2)      # for data pre-processing
 library(fitdistrplus)  # for fitting distributions
 library(markovchain)   # for fitting distributions
 library(dplyr)         # for data processing
+library(ggplot2)       # for plotting
 
 # set file path
 # NOTE: set this path to the folder on your personal machine which contains the downloaded data 
@@ -32,7 +33,7 @@ path <- '/Users/rqo5125/Library/Mobile Documents/com~apple~CloudDocs/Documents/R
 
 # set directories
 maindir <- path                                    # main directory
-datadir <- paste(path, '/hydrodata/', sep = '')   # directory for storing the hydroclimatic data
+datadir <- paste(path, '/hydrodata/', sep = '')    # directory for storing the hydroclimatic data
 rdatadir <- paste(path, '/rdatafiles/', sep = '')  # directory for storing rdata files
 
 # OPTIONAL: create an output directory
@@ -369,7 +370,6 @@ for (i in 1:12) {
   avgwaterusePC[[i]] <- aggregate(avgwateruse$x, by = list(avgwateruse$cluster), FUN = mean)
 }
 
-# save rdata file
 setwd(rdatadir)
 save.image('waterconsumptioncalc.rdata')
 
@@ -396,7 +396,7 @@ for (i in 2:4018) {
   Smod[i] <- Smod[i-1] + P[i] + Q[i] - W[i] - E[i] - L[i]
 }
 
-nrmse(Smod, S[1:4018]) # check accuracy of data-drive water balance
+nrmse(Smod, S[1:4018]) # check accuracy of data-driven water balance
 
 alldata <- data.frame(S[1:4018], P[1:4018], Q[1:4018], W[1:4018], E, L[1:4018]) # 1/1/2010 - 12/31/2020
 names(alldata) <- c('S','P','Q','W','E','L')
@@ -484,16 +484,181 @@ for (i in 1:3) {
 setwd(rdatadir)
 save.image('distributions.rdata')
 
-########## FIGURES ##################
+########## FIGURES AND TABLES ##################
 
+# read in results from ABM model
+setwd(maindir)
+abmresults <- read.csv('experimentresults.csv', skip = 6, stringsAsFactors=FALSE)
 
+# read in actual data
+actualdata <- read.csv(paste(maindir, '/inputData/PhoenixWaterBalData.csv', sep = ''))
 
+# data pre-processing
 
+baseline <- abmresults[-c(1:10),which(abmresults[1,] == 'base')]              # baseline scenario: observed proportion of archetypes
+partialpart <- abmresults[-c(1:10),which(abmresults[1,] == 'partial-part')]   # partially participatory scenario: 50% of archetype 3 --> 5
+particpatory <- abmresults[-c(1:10),which(abmresults[1,] == 'part')]          # participatory scenario: 100% of archetype 3 --> 5
+individualistic <- abmresults[-c(1:10),which(abmresults[1,] == 'indiv')]      # individualistic scenario: 100% of archetype 6 --> 2
+concerned <- abmresults[-c(1:10),which(abmresults[1,] == 'concern')]          # concerned scenario: 100% of archetype 7 --> 4
+combined <- abmresults[-c(1:10),which(abmresults[1,] == 'all-changes')]       # combined scenario: all of the above changes (100% of 3 --> 5, 6 --> 2, and 7 --> 4)
 
+# FIGURE 1
 
+# calculate average
+baseline_matrix <- matrix(as.numeric(unlist(baseline)), nrow = 3500, byrow = F)
+average <- rowMeans(baseline_matrix)
 
+# convert matrix to data frame
+baseline_df <- as.data.frame(baseline_matrix)
 
+# rename columns in baseline_df
+names(baseline_df) = gsub(pattern = "V*", replacement = "", x = names(baseline_df))
 
+# prep plotting data
+timestep <- c(1:3500)
+alldata_baseline <- cbind(baseline_df, average, timestep, actualdata[1:3500,1])
+
+# rearrange data to long form for plotting
+plotdata <- data.frame(melt(alldata_baseline, id.vars = 'timestep', variable.name = 'ModelRun', value.name = 'Storage'))
+
+# add in variable for distinguishing average from the rest of the runs
+typerun <- c(rep('model', 350000), rep('Average', 3500), rep('Actual', 3500))
+plotdata <- cbind(plotdata, typerun)
+
+# plot data + save as a pdf
+setwd(outputdir)
+pdf('figure1.pdf', height = 5, width = 12.5)
+ggplot(plotdata) + geom_line(aes(x = timestep, y = Storage/1000000000, group = ModelRun, color = typerun, size = typerun)) +
+  scale_color_manual(values = c('blue','black','#D3D3D3')) + theme_light(base_size = 16) + guides(color = 'none', size = 'none') +
+  xlab('Time Step (Days)') + ylab(expression(paste('Storage (billions of ', m^3, ')', sep = ''))) +
+  scale_size_manual(values = c(1,1,3))
+dev.off()
+
+# FIGURE 2
+
+# convert to matrix
+partialpart_matrix <- matrix(as.numeric(unlist(partialpart)), nrow = 3500, byrow = F)
+particpatory_matrix <- matrix(as.numeric(unlist(particpatory)), nrow = 3500, byrow = F)
+individualistic_matrix <- matrix(as.numeric(unlist(individualistic)), nrow = 3500, byrow = F)
+concerned_matrix <- matrix(as.numeric(unlist(concerned)), nrow = 3500, byrow = F)
+combined_matrix <- matrix(as.numeric(unlist(combined)), nrow = 3500, byrow = F)
+
+# calculate average
+partialpart_average <- rowMeans(partialpart_matrix)
+particpatory_average <- rowMeans(particpatory_matrix)
+individualistic_average <- rowMeans(individualistic_matrix)
+concerned_average <- rowMeans(concerned_matrix)
+combined_average <- rowMeans(combined_matrix)
+
+# convert matrix to data frame
+partialpart_df <- as.data.frame(partialpart_matrix)
+particpatory_df <- as.data.frame(particpatory_matrix)
+individualistic_df <- as.data.frame(individualistic_matrix)
+concerned_df <- as.data.frame(concerned_matrix)
+combined_df <- as.data.frame(combined_matrix)
+
+# rename columns
+names(partialpart_df) = gsub(pattern = "V*", replacement = "", x = names(partialpart_df))
+names(particpatory_df) = gsub(pattern = "V*", replacement = "", x = names(particpatory_df))
+names(individualistic_df) = gsub(pattern = "V*", replacement = "", x = names(individualistic_df))
+names(concerned_df) = gsub(pattern = "V*", replacement = "", x = names(concerned_df))
+names(combined_df) = gsub(pattern = "V*", replacement = "", x = names(combined_df))
+
+# prep plotting data
+timestep <- c(1:3500)
+alldata_modelruns <- rbind(cbind(baseline_df, timestep),
+                           cbind(partialpart_df, timestep),
+                           cbind(particpatory_df, timestep),
+                           cbind(individualistic_df, timestep),
+                           cbind(concerned_df, timestep),
+                           cbind(combined_df, timestep))
+scenarios <- c(rep('1baseline', 3500), rep('2partialpart', 3500), rep('3part', 3500), rep('4indiv', 3500), rep('5concerned', 3500), rep('6allchanges', 3500))
+alldata_modelruns <- cbind(alldata_modelruns, scenarios)
+
+alldata_averages <- data.frame(average, partialpart_average, particpatory_average, individualistic_average, concerned_average, combined_average, timestep)
+
+# rearrange data to long form for plotting
+plotdata_modelruns <- data.frame(melt(alldata_modelruns, id.vars = c('timestep','scenarios'), variable.name = 'ModelRun', value.name = 'Storage'))
+plotdata_averages <- data.frame(melt(alldata_averages, id.vars = c('timestep'), variable.name = 'ModelRun', value.name = 'Storage'))
+
+plotdata_averages <- cbind(plotdata_averages, scenarios)
+
+# other data for labeling plots
+scenario_names <- c('1baseline'="Baseline", '2partialpart'="Partial Participation",
+  '3part'="Full Participation", '4indiv'="Individualistic", '5concerned' = "Concerned", '6allchanges'="All Changes")
+
+# plot model runs data + save as a pdf
+setwd(outputdir)
+pdf('figure2a.pdf', height = 6, width = 12.5)
+ggplot(plotdata_modelruns) + geom_line(aes(x = timestep, y = Storage/1000000000, group = ModelRun, color = scenarios)) +
+  scale_color_manual(values = c('black','#66c2a5','#8da0cb','#e78ac3','#a6d854','#ffd92f')) + theme_light(base_size = 16) + 
+  xlab('Time Step (Days)') + ylab(expression(paste('Storage (billions of ', m^3, ')', sep = ''))) +
+  facet_wrap(~ scenarios, labeller=as_labeller(scenario_names)) + guides(color = 'none')
+dev.off()
+
+# plot averagedata + save as a pdf
+setwd(outputdir)
+pdf('figure2b.pdf', height = 4.5, width = 12.5)
+ggplot(plotdata_averages) + geom_line(aes(x = timestep, y = Storage/1000000000, group = ModelRun, color = scenarios), size = 1) +
+  scale_color_manual('Scenario', values = c('black','#66c2a5','#8da0cb','#e78ac3','#a6d854','#ffd92f'), 
+                     labels = c('Baseline','Partial Participation', 'Full Participation', 'Individualistic','Concerned','All Changes')) + 
+  theme_light(base_size = 16) + xlab('Time Step (Days)') + ylab(expression(paste('Storage (billions of ', m^3, ')', sep = ''))) +
+  theme(legend.position = "bottom")
+dev.off()
+
+# TABLE 2
+
+# calculate measures of model performance 
+
+nrmse_calc <- c()
+r2_calc <- c()
+
+for (i in 1:100) {
+  # NRSME
+  nrmse_calc[i] <- nrmse(as.numeric(baseline[,i]), actualdata[1:3500,1], norm = 'maxmin') 
+  
+  # R^2
+  r2_calc[i] <-  gof(as.numeric(baseline[,i]), actualdata[1:3500,1])[17]
+}
+
+# store values
+modperf <- data.frame(measure = c('nrmse','r-squared'), minimum = c(min(nrmse_calc), min(r2_calc)), average = c(mean(nrmse_calc), mean(r2_calc)), maximum = c(max(nrmse_calc), max(r2_calc)))
+
+# save csv
+setwd(outputdir)
+write_csv(modperf, 'modelperformance.csv')
+
+# TABLE 3
+
+# store measures of interest (min/mean/max)
+scenario_results <- data.frame(scenario = c('baseline', 'partial_part', 'part', 'indiv', 'concerned', 'combined'), 
+                               minimum = c(min(baseline_df), min(partialpart_df), min(particpatory_df), min(individualistic_df), min(concerned_df), min(combined_df)),
+                               average = c(mean(as.matrix(baseline_df)), mean(as.matrix(partialpart_df)), mean(as.matrix(particpatory_df)), mean(as.matrix(individualistic_df)),
+                                           mean(as.matrix(concerned_df)), mean(as.matrix(combined_df))),
+                               maximum = c(max(baseline_df), max(partialpart_df), max(particpatory_df), max(individualistic_df), max(concerned_df), max(combined_df)))
+
+# save csv
+setwd(outputdir)
+write_csv(scenario_results, 'scenarioresults.csv')
+
+# statistical tests
+
+# get average for each scenario
+all_averages <- data.frame(cbind(average, rowMeans(partialpart_df), rowMeans(particpatory_df), rowMeans(individualistic_df), rowMeans(concerned_df), rowMeans(combined_df)))
+names(all_averages) <- c('baseline','partialpart','part','indiv','concerned','allchanges')
+
+# t test: Are the scenarios statistically significantly different from the baseline
+for (i in 2:6) {
+  print(names(all_averages)[i])
+  print(t.test(all_averages[,1],all_averages[,i]))
+}
+
+#RESULTS: 
+# partialpart --> p-value = 0.000003021 < 0.05 therefore reject the null, difference in means is NOT 0
+# part --> p-value < 0.00000000000000022 < 0.05 therefore reject the null, difference in means is NOT 0
+# indiv --> p-value = 0.9038 therefore fail to reject the null, difference in means is 0
+# concerned --> p-value = 0.8409 therefore fail to reject the null, difference in means is 0
+# allchanges --> p-value < 0.00000000000000022 < 0.05 therefore reject the null, difference in means is NOT 0
 
 
 
